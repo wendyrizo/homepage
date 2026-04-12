c9971b4477c75275aca21071734473ce21f50cda
Wendy Rizo <wendymartinezrizo@gmail.com>
2026-04-11 19:05:39 -0600

    Add Projects section with project cards, update Link component

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+export type Project = {
+  name: string;
+  description: string;
+  link: string;
+};
+
+export const projects: Project[] = [
+  {
+    name: "test",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
+    link: "#",
+  },
+  {
+    name: "test-n",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
+    link: "#",
+  },
+];
