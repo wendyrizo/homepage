b0bd481d5ab124d3a1d12714150dd74429f89fbf
Wendy Rizo <wendymartinezrizo@gmail.com>
2026-04-18 22:19:54 -0600

    Update project card UI: add preview, center icon and text column, improve responsiveness

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,6 @@
 export type Project = {
-  image: string;
+  preview?: string;
+  icon?: string;
   name: string;
   description: string;
   link: string;
@@ -7,14 +8,16 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    image: "",
+    preview: "",
+    icon: "",
     name: "test",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
     link: "#",
   },
   {
-    image: "",
+    preview: "",
+    icon: "",
     name: "test-n",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
